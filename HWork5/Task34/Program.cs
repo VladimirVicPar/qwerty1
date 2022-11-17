4c759a113655971bb6e08f2a8f74ac8b2b90f575
Vladimir Par <rzn-pvv@mail.ru>
2022-11-18 01:11:38 +0300

    Commite Task34

---
+++ HWork5/Task34/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+//Cоздание массива.
+
+int[] creadeArray(int n)
+{
+    int[] array = new int[n];
+    return array;
+}
+
+//Заполнение массива рандомными значениями.
+
+int[] fillArray(int[] array, int min, int max)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(min, max);
+    }
+    return array;
+}
+
+//Печать массива. 
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+//Метод подсчета четных чисел в массиве.
+
+int countEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+//Решение.
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = creadeArray(n);
+array = fillArray(array, 100, 999);
+printArray(array);
+Console.WriteLine($"-> {countEven(array)}");
+
