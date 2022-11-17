9c802062db98404b440b846697742ea94e9f03b1
Vladimir Par <rzn-pvv@mail.ru>
2022-11-17 23:18:17 +0300

    Commit Task25

---
+++ HWork4/Task25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Expo(int A, int B);
+{
+    int n = A;
+    for (int i = 2; i <= B; i++)
+    n = n * A;
+    return n; 
+}
+
+// Решение
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Число {a}, возведенное в степень {b} равно: {Expo(a,b)}"); 
