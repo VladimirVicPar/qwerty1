bf9410a0c2b2974b3d87628a15c20c1d76be1d85
Vladimir Par <rzn-pvv@mail.ru>
2022-11-17 23:31:01 +0300

    Commit Task27

---
+++ HWork4/Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int SumNumbers(int num)
+{
+    int n = num;
+    int rev = 0;
+    while (n != 0)
+    {
+        int r = n % 10;
+        rev = rev + r;
+        n = n / 10;
+    }
+    return rev;
+}
+
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в числе {N} равна: {SumNumbers(N)}");
