2e5a9a0b5faf55df90b5ff12a06696f4e3799195
Vladimir Par <rzn-pvv@mail.ru>
2022-11-18 16:47:59 +0300

    Commite Task38

---
+++ HWork5/Task38/Program.cs
@@ -0,0 +1,76 @@
+﻿//  Задача 38: Задайте массив вещественных чисел.
+//  Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+//Cоздание массива.
+
+double[] creadeArray(int n)
+{
+    double[] array = new double[n];
+    return array;
+}
+
+//Заполнение массива рандомными значениями.
+
+double[] fillArray(double[] array, double min, double max)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        var next = rand.NextDouble();
+        array[i] = min + next * (max - min);
+    }
+    return array;
+}
+
+//Печать массива. 
+
+void printArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+//Нахождение максимального элемента в массиве.
+
+double maxValue(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+return max;
+}
+
+//Нахождение минимального элемента в массиве.
+
+double minValue(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+return min;
+}
+
+//Решение.
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[] array = creadeArray(n);
+array = fillArray(array, 0, 100);
+printArray(array);
+Console.WriteLine($"-> {maxValue(array) - minValue(array)}");
